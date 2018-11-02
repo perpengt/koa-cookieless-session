608c5dc95b2be89e5c32781fe859ca175ebb0832
Yongbin Kim <rp@redpumpkin.net>
2018-11-02 19:07:25 +0900

    remove dist

--- dist/memory_store.d.ts
+++
@@ -1,6 +0,0 @@
-import { Store } from './store';
-export default class MemoryStore implements Store {
-    get(key: string): Promise<any>;
-    set(key: string, value: any): Promise<void>;
-    destroy(key: string): Promise<void>;
-}
