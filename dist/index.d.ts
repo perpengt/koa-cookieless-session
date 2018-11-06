90ec798a9d0e55861aa9c912144d1afd23b214da
Yongbin Kim <rp@redpumpkin.net>
2018-11-06 17:13:48 +0900

    Remove dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Koa from 'koa';
-import { SessionOpts } from './opts';
-declare const session: (app: Koa, opts?: Partial<SessionOpts> | undefined) => (ctx: Koa.Context, next: () => Promise<void>) => Promise<void>;
-export default session;
--- dist/store.d.ts
+++
@@ -1,6 +0,0 @@
-import { SessionOpts } from './opts';
-export interface Store {
-    get(sessID: string): Promise<object>;
-    set(sessID: string, data: any, maxAge: SessionOpts['maxAge']): Promise<void>;
-    destroy(sessID: string): Promise<void>;
-}
