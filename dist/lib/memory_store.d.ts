5a222752ce6d91f9a0a131b9f1c8d458ed57cc58
Yongbin Kim <rp@redpumpkin.net>
2018-11-06 15:50:31 +0900

    잘못 생성된 파일 제거

--- dist/lib/memory_store.d.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from './store';
-export default class MemoryStore implements Store {
-    constructor();
-    get(key: string): Promise<any>;
-    set(key: string, value: any): Promise<void>;
-    destroy(key: string): Promise<void>;
-}
--- dist/lib/opts.d.ts
+++
@@ -1,7 +0,0 @@
-import { Store } from './store';
-export interface SessionOpts {
-    maxAge: number;
-    store: Store | (() => Store);
-    autoCommit: boolean;
-    rolling: boolean;
-}
